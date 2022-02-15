2d16b421d1000dc62b1663b9b4f3b7e94841e8ee
arjungautam1 <arjungautam5431@gmail.com>
2022-02-15 11:57:54 +0545

    Initial Commit

---
+++ Valentine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Valentine
+//
+//  Created by Arjun Gautam on 15/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Valentine/ValentineApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValentineApp.swift
+//  Valentine
+//
+//  Created by Arjun Gautam on 15/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct ValentineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
